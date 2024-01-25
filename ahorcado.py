6e74566a01aa494fbaa6b6c3e99e6ec71622d3b7
guillegafas <116709250+guillegafas@users.noreply.github.com>
2024-01-25 11:04:37 +0100

    Add files via upload

--- ahorcado.py
+++ ahorcado.py
@@ -26,9 +26,13 @@ f = str(total_palabras[z])
 print(f)
 palabra = []
 intento = []
+
 for caracter in f:
     palabra.append(caracter)
-print
+print(palabra)
+del palabra[0:2]
+del palabra[-3:]
+print(palabra)
 for i in range(len(palabra)):
     intento.append(' ')
 print(intento)
@@ -41,4 +45,11 @@ while palabra != intento:
     if l not in palabra:
         print('letra incorrecta')
     print(intento)
+palabra_final =  ''.join(intento)
+print(palabra_final)
 print('Game over')
+
+cursor = mydb.cursor()
+cursor.execute("INSERT INTO INTENTOS (palabra_adivinada) VALUES (%s)", (palabra_final,))
+mydb.commit()
+mydb.close()
